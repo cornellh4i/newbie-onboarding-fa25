f01440e489de8f83fed01f33695c793e1b6e6df9
Nihal Nagdev <nagdevnihal@gmail.com>
2025-10-30 01:14:47 +0530

    Fix: Corrected alignment of the top bar content on desktop layout for issue #9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8
             backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border
             lg:bg-gray-200 lg:p-4 dark:border-neutral-800 dark:bg-zinc-800/30
-            dark:from-inherit lg:dark:bg-zinc-800/30"
+            dark:from-inherit lg:dark:bg-zinc-800/30 lg:text-left"
         >
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
